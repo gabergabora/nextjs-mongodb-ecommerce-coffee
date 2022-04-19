f7589e7e54e7d794b58684ec73950d7a34595096
kamal heydari <kamal3heydari@gmail.com>
2022-04-19 14:21:08 +0430

    feat custome font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pacifico: "Pacifico",
+      },
+      backgroundImage: { "banner": "url(/images/banner.jpg)" },
+    },
   },
   plugins: [],
-}
+};
