5084c75cd4bbcc8cbcd433bec5b5469704c248bc
kamal heydari <kamal3heydari@gmail.com>
2022-04-25 20:16:09 +0430

    feat footer  component

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return(
+        <footer className="h-[10vh] bg-zinc-900">
+            <div className="text-center section-container">
+                <p>developed by kamal heydari</p>
+            </div>
+        </footer>
+    )
+}
